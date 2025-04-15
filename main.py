be173dc53e643603db83f651143ed1362f9d0ed5
Zoha Aslam <160209815+zohaaslam@users.noreply.github.com>
2025-04-15 12:25:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,11 +9,95 @@ from base64 import urlsafe_b64encode
 from hashlib import pbkdf2_hmac
 
 
+# === data information of user ===
+DATA_FILE = "secure_data.json"
+SALT = b"secure_salt_value"
+LOCKOUT_DURATION = 60
 
 
 
+# === section login details ===
+if "authenticated_user" not in st.session_state:
+    st.session_state.authenticated_user = None
+
+if "failed_attempts" not in st.session_state:
+    st.session_state.failed_attempts = 0   
 
 
+if "lockout_time" not in st.session_state:
+    st.session_state.lockout_time = 0
+
+
+
+# === if data is load ===
+def load_data():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+        
+    return{}    
+
+
+def save_data(data):
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f)
+
+
+
+def generate_key(passkey):
+    key = pbkdf2_hmac('sha256' , passkey.encode(), SALT, 100000)     
+    return urlsafe_b64encode(key)
+
+def hash_password(password):
+    return hashlib.pbkdf2_hmac('sha256', password.encode(), SALT, 100000).hex()
+
+
+# === cryptography.fernet used ===
+def encrypt_text(text, key):
+    cipher = Fernet(generate_key(key))
+    return cipher.encrypt(text.encode()).decode()
+
+
+def decrypt_text(encrypt_text,key):
+    try:
+        cipher = Fernet(generate_key(key)) 
+        return cipher .decrypt(encrypt_text.encode()).decode()
+    except:
+        return None
+
+
+stored_data  = load_data()
+
+
+#  === navigation bar ====
+st.title("🔐 Secure Login System")
+menu = ["Home","Register","Login","Store Data","Retrieve Data"]
+choice = st.sidebar.selectbox("Navigation", menu)
+
+
+if choice == "Home":
+    st.subheader(" Welcome To My 🔐 Data Encryption System Using Streamlit !")
+    st.markdown("Develop a Streamlit-based secure data storage and retrieval system where:Users store data  with a unique passkey.users decryte data by providing the correct passkey.Multiple failed attempt result in a forced reauthorization (login page).the system operators entirely in memory without external databases. ")
+
+
+# === user registeration ===
+elif choice == "Register":
+    st.subheader("✏️ Register New User")
+    username = st.text_input("Choose Username")
+    password = st.text_input("Choose Password", type="password")
+
+    if st.button("Register"):
+        if username in stored_data:
+            st.warning("⚠️User already exisits.")
+        else:
+            stored_data[username] =  {
+                "password": hash_password(password),
+                "data" : []
+            }
+            save_data(stored_data)
+            st.success("✅ User registered successfully!")
+    else:
+        st.error("Both fields are required.")    
 
 elif choice == "Login":
     st.subheader("🔑 User Login")
