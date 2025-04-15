67073a8d738b005c564ca09aa164978c9ed4b0ef
Zoha Aslam <160209815+zohaaslam@users.noreply.github.com>
2025-04-16 00:16:14 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,65 @@
+elif choice == "Login":
+    st.subheader("🔑 User Login")
+
+    if time.time() < st.session_state.lockout_time:
+        remaining = int(st.session_state.lockout_time - time.time())
+        st.error(f"⏱️Too many failed attempts. Please wait {remaining} seconds.")
+        st.stop()
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if username in stored_data and stored_data[username]["password"] == hash_password(password):
+            st.session_state.authenticated_user = username
+            st.session_state.failed_attempts = 0
+            st.success(f"😊 Welcome {username}!")
+        else:
+            st.session_state.failed_attempts += 1
+            remaining = 3 - st.session_state.failed_attempts
+            st.error(f"❌ Invalid Credential! Attempts left: {remaining}")
+
+            if st.session_state.failed_attempts >= 3:
+                st.session_state.lockout_time = time.time() + LOCKOUT_DURATION
+                st.error("🛑Too many failed attempts. Locked for 60 seconds")
+                st.stop()
+
+elif choice == "Store Data":
+    if not st.session_state.authenticated_user:
+        st.warning("🔐Please login first.")
+    else:
+        st.subheader("Store Encrypted Data")
+        data = st.text_area("Enter data to encrypt")
+        passkey = st.text_input("Encryption key (passphrase)", type="password")
+
+        if st.button("Encrypt And Save"):
+            if data and passkey:
+                encrypted = encrypt_text(data, passkey)
+                stored_data[st.session_state.authenticated_user]["data"].append(encrypted)
+                save_data(stored_data)
+                st.success("✅ Data encrypted and saved successfully!")
+            else:
+                st.error("All fields are required to fill.")
+
+elif choice == "Retrieve Data":
+    if not st.session_state.authenticated_user:
+        st.warning("🔓Please login first.")
+    else:
+        st.subheader("🔎Retrieve Data")
+        user_data = stored_data.get(st.session_state.authenticated_user, {}).get("data", [])
+        if not user_data:
+            st.info("No Data Found!")
+        else:
+            st.write("Encrypted Data Entries:")
+            for i, item in enumerate(user_data):
+                st.code(item, language="text")
+
+            encrypted_input = st.text_area("Enter Encrypted Text")
+            passkey = st.text_input("Enter Passkey To Decrypt", type="password")
+
+            if st.button("Decrypt"):
+                result = decrypt_text(encrypted_input, passkey)
+                if result:
+                    st.success(f"✅ Decrypted: {result}")
+                else:
+                    st.error("❌ Incorrect passkey or corrupted data.")
